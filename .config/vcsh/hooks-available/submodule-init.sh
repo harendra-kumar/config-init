a43ee5afa2593947fd437538734f3fe45b6bf43c
Harendra Kumar <harendra.kumar@gmail.com>
2016-06-22 20:35:12 +0530

    test

--- .config/vcsh/hooks-available/submodule-init.sh
+++ .config/vcsh/hooks-available/submodule-init.sh
@@ -4,4 +4,6 @@
 # conflict. We have a copy of .gitmodules in .config/repo-name instead.
 # So we checkout the submodule from ~/.config/repo-name instead of form home.
 unset GIT_WORK_TREE
+echo "~/.config/$VCSH_REPO_NAME"
+ls -al ~/.config/$VCSH_REPO_NAME
 vcsh $VCSH_REPO_NAME --work-tree ~/.config/$VCSH_REPO_NAME submodule update --init --recursive
