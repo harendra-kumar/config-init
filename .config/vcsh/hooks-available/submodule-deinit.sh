4cd5b5bbad9f7008ad16d3da54690cddd3ae58c9
Harendra Kumar <harendra.kumar@gmail.com>
2016-06-22 20:25:19 +0530

    Use different work-tree for submodules

--- .config/vcsh/hooks-available/submodule-deinit.sh
+++ .config/vcsh/hooks-available/submodule-deinit.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# See comments in the submodule init script about work-tree dir change here
+unset GIT_WORK_TREE
 if test "$VCSH_COMMAND" = "delete"
 then
-  vcsh $VCSH_REPO_NAME submodule deinit .
+  vcsh $VCSH_REPO_NAME --work-tree ~/.config/$VCSH_REPO_NAME submodule deinit .
 fi
--- .config/vcsh/hooks-available/submodule-init.sh
+++ .config/vcsh/hooks-available/submodule-init.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-vcsh $VCSH_REPO_NAME submodule update --init --recursive
+# We hide the .gitmodules in the repo root so that multiple repos do not have a
+# conflict. We have a copy of .gitmodules in .config/repo-name instead.
+# So we checkout the submodule from ~/.config/repo-name instead of form home.
+unset GIT_WORK_TREE
+vcsh $VCSH_REPO_NAME --work-tree ~/.config/$VCSH_REPO_NAME submodule update --init --recursive
